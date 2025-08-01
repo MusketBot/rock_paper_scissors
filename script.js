692774c9ddb33c77a5d931b8ac025e720dbf9bf3
Captian Script <117399684+MusketBot@users.noreply.github.com>
2025-08-01 10:38:34 -0400

    Added logic to play game

--- script.js
+++ script.js
@@ -4,7 +4,48 @@ const choice = ['rock', 'paper', 'scissors'];
 
 //getting computer choice.
 function getComputerChoice() {
-    let generateNum = Math.floor(Math.random() * 3);
-    return choice[generateNum];
+  let generateNum = Math.floor(Math.random() * 3);
+  return choice[generateNum];
 }
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let answer = prompt('What is your choice? (rock, paper, or scissors)').toLowerCase();
+    while (!choice.includes(answer)) {
+      answer = prompt('Invalid choice. Enter: "rock", "paper", "scissors"').toLowerCase();
+    } 
+    return answer;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return `A tie! Both chose ${humanChoice}.`;
+    }
+  
+    if ( 
+        (humanChoice === choice[0] && computerChoice === choice[2]) ||
+        (humanChoice === choice[2] && computerChoice === choice[1]) ||
+        (humanChoice === choice[1] && computerChoice === choice[0])
+    ) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}. Your score: ${humanScore}, Computer score: ${computerScore}`;
+    } else {
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${humanChoice}. Your Score: ${humanScore}, Computer score: ${computerScore}`;
+    }
+}
+
+function playGame() {
+    for (let round = 1; round <= 5; round++) {
+      console.log(`Round ${round}:`);
+      const humanSelection = getHumanChoice();
+      const computerSelection  = getComputerChoice();
+      console.log(playRound(humanSelection, computerSelection));
+    }
+    if (humanScore > computerScore) {
+      return `Game over! You win the game! Final score - You: ${humanScore}, Computer: ${computerScore}`;
+    } else if (humanScore< computerScore) {
+      return `Game over! You lose the game! Final score - You: ${humanScore}, Computer score: ${computerScore}`;
+    } else {
+      return `Game over! It's a tie! Final score - You: ${humanScore}, Computer: ${computerScore}`;
+    }
+}
+console.log(playGame());
